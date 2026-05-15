f1fb4db8f42fe747b537a47a832ad163c1e379aa
fulldump <gerardooscarjt@gmail.com>
2026-05-15 12:04:35 +0200

    feat: add config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,11 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/fulldump/goconfig"
+
 	"minecloud/pkg/network"
 	"minecloud/pkg/web"
 )
 
+type Config struct {
+	Addr string `usage:"Port to listen on"`
+}
+
 func main() {
+
+	config := &Config{
+		Addr: ":8080",
+	}
+	goconfig.Read(config)
+
 	if err := network.Initialize(); err != nil {
 		log.Fatal(err)
 	}
@@ -16,10 +28,10 @@ func main() {
 	http.Handle("/", http.FileServer(http.FS(web.MustStaticFS())))
 	http.HandleFunc("/ws", network.HandleWebSocket)
 
-	log.Println("Server started on http://localhost:8080")
-	log.Println("WebSocket endpoint: ws://localhost:8080/ws")
+	log.Println("Server started on", config.Addr)
+	log.Println("WebSocket endpoint: " + config.Addr + "/ws")
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(config.Addr, nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module minecloud
 
 go 1.21
 
-require github.com/gorilla/websocket v1.5.1
+require (
+	github.com/fulldump/goconfig v1.8.0
+	github.com/gorilla/websocket v1.5.1
+)
 
 require golang.org/x/net v0.17.0 // indirect
